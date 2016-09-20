c215428301cbf4d3641f664db9803a0f8e0154be
celinewsm <Celine@Celines-MacBook-Pro.local>
2016-09-20 21:35:28 +0800

    adding profile image

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ var passport = require('../config/ppConfig')
 var isLoggedIn = require('../middleware/isLoggedIn')
 var router = express.Router()
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: 'static/img/uploads/' })
 
 router.get('/', isLoggedIn, function (req, res) {
   db.user.findAll().then(function(allUsers) {
@@ -18,8 +18,6 @@ router.get('/', isLoggedIn, function (req, res) {
     // users will be an array of all User instances
       res.render('user/index', {focus: focusList})
   })
-
-  // res.render('user/index')
 })
 
 router.get('/profile', isLoggedIn, function (req, res) {
@@ -61,6 +59,17 @@ router.post('/profile', function (req, res) {
 
 router.post('/profile/upload', upload.single('avatar'), function (req, res, next) {
   console.log("req.file:",req.file)
+
+  db.user.update({
+    profileImg: req.file.path.replace("static/", "/")
+  }, {
+    where: {
+      id: req.session.passport.user
+    }
+  }).then(function (user) {
+    res.redirect('/user/profile')
+  })
+
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
 })
--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
   console.log("main.js connected")
   $('.button-collapse').sideNav()
   $('select').material_select();
+
+  $('#profileImgUpload').change(function(){
+    $('#profileImgForm').submit();
+  })
+
 })
